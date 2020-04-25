6ea0ea0f086b10e6061b2797c03930bdb3890278
Sairam Vinnakota <sai070cm118@gmail.com>
2020-04-25 19:40:51 +0530

    cache time added

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var expireTime = 604800000;
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist',{ maxAge: expireTime}));
 
 app.get('/*', function(req,res) {
     
