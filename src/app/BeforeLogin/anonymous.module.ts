912a1c9d879979da571558b3be1d2b49168d3c2c
Sairam Vinnakota <sai070cm118@gmail.com>
2020-04-25 17:03:07 +0530

    Login module included in one place only

--- src/app/BeforeLogin/anonymous.module.ts
+++ src/app/BeforeLogin/anonymous.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AnonymousRoutingModule } from './anonymous.routing';
 import {  Page2Component } from './page2/page2.component';
-import { LoginComponent} from '../Shared/components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { NgxUiLoaderModule } from 'ngx-ui-loader';
 @NgModule({
   declarations: [
-    Page2Component,
-    LoginComponent
+    Page2Component
   ],
   imports: [
     CommonModule,
